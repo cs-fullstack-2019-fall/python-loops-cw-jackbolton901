c9e1bcf4a9248fb3635961a90bd5e8b9a6944559
jackbolton901 <bltnjck@gmail.com>
2019-08-21 16:14:03 -0500

    08-21-2019_classwork. loops

---
+++ cw08-12.py
@@ -0,0 +1,19 @@
+# exercise 1
+for num in range(-20,51,1):
+    print(num)
+# exercise 2
+
+for num2 in range(0, 21, 2):
+    print(num2)
+# exercise 3
+
+number1=int(input("enter number"))
+number2=int(input("enter number"))
+number3=int(input("enter number"))
+theAverage= ((number1 + number2 + number3)/3)
+print("The average of " + str(number1,)+", " +str(number2) +", and " +str(number3)+ " is "+ str(theAverage)+ ".")
+
+# exercise 4
+
+for var in range(0,101, 4):
+    print(var)
